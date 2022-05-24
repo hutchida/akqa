a9245662298e9b116074f8289b9a7790053d8577
Dan <danielmhutchings@gmail.com>
2022-05-24 11:15:09 +0200

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './Components/Home';
 import Demo from './Components/Demo';
@@ -19,8 +18,3 @@ root.render(
   </Router>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
